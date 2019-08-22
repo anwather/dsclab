ff7610a8bcfb669a29a2d46ebff7122ba2b6acb8
Anthony Watherston <anwather@microsoft.com>
2019-08-22 16:48:38 +1000

    testin path changes

--- Pipeline/1_UploadConfiguration.ps1
+++ Pipeline/1_UploadConfiguration.ps1
@@ -1,9 +1,10 @@
-Enable-AzureRMAlias -Verbose
+Enable-AzureRMAlias
 $ResourceGroupName = $env:ResourceGroupName
 $automationAccountName = $env:AutomationAccountName
 
+Set-Location $env:SYSTEM_DEFAULTWORKINGDIRECTORY\Pipeline
+
 $automationAccount = Get-AzureRmAutomationAccount -ResourceGroupName $ResourceGroupName -Name $automationAccountName
-Write-Output $automationAccount.AutomationAccountName
 $configurations = Get-ChildItem -Path ..\Configurations -Filter *.ps1 -Exclude ConfigurationData.ps1 -File -Recurse
 
 foreach ($configuration in $configurations) {
