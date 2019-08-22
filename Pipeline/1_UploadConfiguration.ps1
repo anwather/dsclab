50d3482bfd339ba1c5111193c0f9925871e0debe
Anthony Watherston <anwather@microsoft.com>
2019-08-22 16:45:02 +1000

    updating script

--- Pipeline/1_UploadConfiguration.ps1
+++ Pipeline/1_UploadConfiguration.ps1
@@ -1,9 +1,9 @@
-Enable-AzureRMAlias
+Enable-AzureRMAlias -Verbose
 $ResourceGroupName = $env:ResourceGroupName
 $automationAccountName = $env:AutomationAccountName
 
 $automationAccount = Get-AzureRmAutomationAccount -ResourceGroupName $ResourceGroupName -Name $automationAccountName
-
+Write-Output $automationAccount.AutomationAccountName
 $configurations = Get-ChildItem -Path ..\Configurations -Filter *.ps1 -Exclude ConfigurationData.ps1 -File -Recurse
 
 foreach ($configuration in $configurations) {
